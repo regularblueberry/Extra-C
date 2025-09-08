a0279f7ddb7cb8710e8cc7a8ed6d6b849222d805
blueberry <blueberryprograms@gmail.com>
2025-09-08 04:48:34 -0400

    xc-core: created unity build file

---
+++ libs/ExtraC/core/build.c
@@ -0,0 +1,35 @@
+#include "./src/data/DSN.c"
+#include "./src/data/list.c"
+#include "./src/data/queue.c"
+#include "./src/data/stack.c"
+#include "./src/data/structs.c"
+#include "./src/data/map.c"
+#include "./src/data/base-types.c"
+
+
+#include "./src/math/numbers.c"
+
+
+#include "./src/error/logger.c"
+#include "./src/error/errorcodes.c"
+
+
+#include "./src/malloc/pool.c"
+#include "./src/malloc/arena.c"
+#include "./src/malloc/buffer.c"
+#include "./src/malloc/c_malloc.c"
+
+#include "./src/string/scan.c"
+#include "./src/string/format.c"
+#include "./src/string/strings.c"
+#include "./src/string/strbuilder.c"
+#include "./src/string/regex.c"
+
+
+#include "./src/time/time.c"
+#include "./src/time/watch.c"
+
+#include "./src/Class.c"
+
+
+
